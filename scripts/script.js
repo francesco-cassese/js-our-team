28640fd6cc518b80a05212b70ae91b9133018478
Francesco Cassese <francescocassese9@gmail.com>
2026-04-10 17:10:52 +0200

    feat: richiamata funzione stampaCard in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,5 @@ const inputNome = document.querySelector('#input-name');
 const inputRole = document.querySelector('#input-role');
 const inputImg = document.querySelector('#input-img');
 const inputEmail = document.querySelector('#input-email');
+
+cardContainer.innerHTML += stampaCard(teamMembers);
