79c315e8f14aa1f94ad9d036bd7174bb64ce00bf
Francesco Cassese <francescocassese9@gmail.com>
2026-04-10 16:55:15 +0200

    feat: aggiunta funzione creaCard in function.js

--- scripts/function.js
+++ scripts/function.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// FUNZIONE CREA CARD
+
+const creaCard = impiegato => {
+
+    const cardImpiegato = `
+    <div class="card">
+                    <div class="container-img">
+                        <img src="${impiegato.img}" alt="Img-dipendente" class="img-card">
+                    </div>
+                    <div class="user-info">
+                        <h3>NOME: ${impiegato.name}</h3>
+                        <span>RUOLO: ${impiegato.role}</span>
+                        <span>EMAIL: ${impiegato.email}</span>
+                    </div>
+                </div>`;
+    return cardImpiegato;
+}
