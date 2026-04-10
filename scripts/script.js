600d7b9b1a23afc8063d1f413a23b7c59fd42642
Francesco Cassese <francescocassese9@gmail.com>
2026-04-10 17:46:13 +0200

    fix: nome classe form

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const cardContainer = document.querySelector('.container-cards');
-const form = document.querySelector('#form-work-with-us');
+const form = document.querySelector('#form-work-whit-us');
 const inputNome = document.querySelector('#input-name');
 const inputRole = document.querySelector('#input-role');
 const inputImg = document.querySelector('#input-img');
 const inputEmail = document.querySelector('#input-email');
 
-cardContainer.innerHTML += stampaCard(teamMembers);
+cardContainer.innerHTML = stampaCard(teamMembers);
+
+form.addEventListener('submit', aggiungiCard);
