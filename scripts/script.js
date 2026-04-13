0ff7ba521f4aad9d9bdda7e96361c9979d8fdad2
Francesco Cassese <francescocassese9@gmail.com>
2026-04-13 14:31:48 +0200

    docs: aggiunta commenti esplicativi alla logica del DOM

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,8 @@ const inputRole = document.querySelector('#input-role');
 const inputImg = document.querySelector('#input-img');
 const inputEmail = document.querySelector('#input-email');
 
-cardContainer.innerHTML = stampaCard(teamMembers);
+cardContainer.innerHTML = stampaCard(teamMembers);                   // Eseguo la prima stampa delle card all'avvio
 
-form.addEventListener('submit', aggiungiCard);
+form.addEventListener('submit', aggiungiCard);                       // Resto in ascolto del "submit" per aggiungere un impiegato
 
-cardContainer.addEventListener('click', gestisciClickEliminazione);
+cardContainer.addEventListener('click', gestisciClickEliminazione);  // Uso l'event delegation per gestire i click sui tasti elimina
